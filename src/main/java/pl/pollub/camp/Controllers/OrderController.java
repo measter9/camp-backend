efccda0c477cfc3b2c1f2ab6e4b8d3098a618f52
pavilion <jakisfajnyktos@gmail.com>
2024-10-16 19:52:58 +0200

    feat: implemented UserController refactor

--- src/main/java/pl/pollub/camp/Controllers/OrderController.java
+++ src/main/java/pl/pollub/camp/Controllers/OrderController.java
@@ -5,37 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.pollub.camp.Models.OrderStatus;
 import pl.pollub.camp.Models.Orders;
-import pl.pollub.camp.Models.Role;
+import pl.pollub.camp.Models.Users;
 import pl.pollub.camp.Repositories.OrderRepository;
 import pl.pollub.camp.Repositories.UserRepository;
-import pl.pollub.camp.Models.Users;
-
-import java.util.Optional;
 
 @Controller
-@RequestMapping(path = "/demo")
-public class MainController {
+@RequestMapping(path = "/order")
+public class OrderController {
     @Autowired
-    private UserRepository userRepository;
-
+    UserRepository userRepository;
     @Autowired
-    private OrderRepository orderRepository;
-
-    @PostMapping(path = "/addUser")
-    public @ResponseBody String addUser(@RequestParam String name, @RequestParam String password, @RequestParam String email){
-        Users u = new Users();
-        u.setName(name);
-        u.setPassword(password);
-        u.setEmail(email);
-        u.setRole(Role.CUSTOMER);
-        userRepository.save(u);
-        return "User succesfully created";
-    }
-
-    @GetMapping(path = "/allUsers")
-    public @ResponseBody Iterable<Users> getAllUsers(){
-        return userRepository.findAll();
-    }
+    OrderRepository orderRepository;
 
     @PostMapping(path = "/addOrder")
     public @ResponseBody String addOrder(@RequestParam String commnet, @RequestParam int userId){
@@ -52,4 +32,5 @@ public class MainController {
     public @ResponseBody Iterable<Orders> getAllOrders(){
         return orderRepository.findAll();
     }
+
 }
---
+++ src/main/java/pl/pollub/camp/Controllers/UserController.java
@@ -0,0 +1,32 @@
+package pl.pollub.camp.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.pollub.camp.Models.Role;
+import pl.pollub.camp.Repositories.UserRepository;
+import pl.pollub.camp.Models.Users;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addUser(@RequestParam String name, @RequestParam String password, @RequestParam String email){
+        Users u = new Users();
+        u.setName(name);
+        u.setPassword(password);
+        u.setEmail(email);
+        u.setRole(Role.CUSTOMER);
+        userRepository.save(u);
+        return "User succesfully created";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Users> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
