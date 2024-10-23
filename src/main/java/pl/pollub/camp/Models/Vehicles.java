c761429a8fe0920601d516368bf3f82551611370
Mariia <golubanatolu@gmail.com>
2024-10-23 19:51:47 +0200

    vehicle

--- src/main/java/pl/pollub/camp/Models/Vehicles.java
+++ src/main/java/pl/pollub/camp/Models/Vehicles.java
@@ -2,29 +2,30 @@ package pl.pollub.camp.Models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
-enum VehicleStatus{
-    AVAILABLE,
-    RENTED,
-    UNAVAILABLE,
-    SERVICE
-}
 
 @Entity
 @Getter
 public class Vehicles {
     @Id
     @GeneratedValue
+    @Setter
     private int Id;
     @Getter
+    @Setter
     private String name;
     @Getter
     private String Description;
     @Getter
+    @Setter
+    private VehicleStatus vehicleStatus;
+    @Getter
+    @Setter
     private String Comment;
     @OneToOne
+    @Setter
     private VehicleType vehicleTypeList;
     @OneToMany
     List<Services> servicesList;
